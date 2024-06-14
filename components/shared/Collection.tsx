2f876373ab48b10edc433930475a4e40fdce752f
Balaganesh K <k.balaganesh26@gmail.com>
2024-06-14 21:26:03 +0530

    chore: Update key prop in Card component to use image._id as any

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -56,7 +56,7 @@ export const Collection = ({
       {images.length > 0 ? (
         <ul className="collection-list">
           {images.map((image) => (
-            <Card image={image} key={image._id} />
+            <Card image={image} key={image._id as any }  />
           ))}
         </ul>
       ) : (
@@ -97,7 +97,7 @@ export const Collection = ({
 const Card = ({ image }: { image: IImage }) => {
   return (
     <li>
-      <Link href={`/transformations/${image._id}`} className="collection-card">
+      <Link href={`/transformations/${image._id}`} className="col-card">
         <CldImage
           src={image.publicId}
           alt={image.title}
